0fd2c8396cf48c5ae1b00d2847ecd4871b535cbf
Prosen-Ghosh <prosenghosh25@gmail.com>
2017-08-12 14:40:53 +0600

    added admin,login,shades ~ login from implemented ~ navbar implemented

--- color.js
+++ color.js
@@ -13,7 +13,7 @@ var generateIndexPage = function() {
             console.log(test);
             html += '<div class="color-box" style="background-color:'+value+';">'+
                     '<hr/>'+
-                    '<h4 class="text-center color-name">'+ key +'</h4>'+
+                    '<a href="shades.html#'+key+'"><h4 class="text-center color-name">'+ key +'</h4></a>'+
                     '<p class="text-center color-code">'+ value +'</p>'+
                     '</div>';
         }
