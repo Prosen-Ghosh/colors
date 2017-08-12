93e6f5322a75a94a2a144922a8cb4986fa39848d
Prosen-Ghosh <prosenghosh25@gmail.com>
2017-08-12 19:08:30 +0600

    url replace function used

--- admin.js
+++ admin.js
@@ -1,12 +1,12 @@
 var ref = new Firebase('https://colors-a8c0c.firebaseio.com/Users');
 
-var isAdmin = function(userName,userPassword){
+var isAdmin = async function(userName,userPassword){
     var obj = {};
-    ref.on('value', function(snap){
+    await ref.on('value', function(snap){
         obj = snap.val();
-        console.log(obj)
+        console.log(obj);
     });
-
+    console.log("a");
     if(obj.username === userName && obj.password === userPassword)return true;
     return false;
 }
@@ -15,8 +15,7 @@ $('#loginBtn').on('click',function(){
     var userName = $('#userName').val();
     var userPassword = $('#userPassword').val();
     if(isAdmin(userName,userPassword)){
-        window.location.href = window.location.origin + "/colors/adminHome.html";
-        console.log(window.location);
+        window.location.replace(window.location.origin + "/colors/adminHome.html");
     }
     else {
         $('.loginError').html('<div class="alert alert-danger text-center" role="alert"><strong>Oops!</strong> User Name Or Password Is Not Correct.</div>');
