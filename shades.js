97f9453cc36ded78bebd9a9b2704b5c12ad262c0
Prosen-Ghosh <prosenghosh25@gmail.com>
2017-08-12 17:10:05 +0600

    shades implemented

--- shades.js
+++ shades.js
@@ -1,1 +1,25 @@
-console.log(window.location.hash.split('#')[1]);
+var url = window.location.hash.split('#')[1]
+var ref = new Firebase('https://colors-a8c0c.firebaseio.com/Colors/'+url);
+
+var generateShadesPage = function() {
+    var id = $('#shades');
+    var html = '';
+    ref.child('shades').on('value', function(snap) {
+       objects = snap.val();
+       console.log(objects)
+       for(var key in objects){
+           var value = objects[key];
+           //console.log(value);
+            html += '<div class="color-box" style="background-color:'+value+';">'+
+            '<hr/>'+
+            '<h4 class="text-center color-name">'+key +'</h4>'+
+            '<p class="text-center color-code">'+ value +'</p>'+
+            '</div>';
+       }
+        id.html(html);     
+    });
+};
+
+$(document).ready(function(){
+    generateShadesPage();
+});
