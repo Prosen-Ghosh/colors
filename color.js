9440848312cbf460ec64872deb84e5cb9a05af88
Prosen-Ghosh <prosenghosh25@gmail.com>
2017-08-12 13:37:37 +0600

    firebase connection done ~ page color box render implemented

---
+++ color.js
@@ -0,0 +1,26 @@
+var ref = new Firebase('https://colors-a8c0c.firebaseio.com/');
+
+
+var generateIndexPage = function() {
+    var id = $('#hero');
+    var html = '';
+    var test = 1;
+    ref.on('value', function(snap) {
+       objects = snap.val();
+        for(var key in objects) {
+            var value = objects[key];
+            console.log(key , value);
+            console.log(test);
+            html += '<div class="color-box" style="background-color:'+value+';">'+
+                    '<hr/>'+
+                    '<h4 class="text-center color-name">'+ key +'</h4>'+
+                    '<p class="text-center color-code">'+ value +'</p>'+
+                    '</div>';
+        }
+        id.html(html);      
+    });
+};
+
+$(document).ready(function(){
+    generateIndexPage();
+});
